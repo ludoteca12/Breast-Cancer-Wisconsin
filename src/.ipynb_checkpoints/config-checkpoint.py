d2684596137a9bc53d7ecb09a1d290795b2dec99
Mateus Vieira <vv.mateus@hotmail.com>
2025-12-02 09:46:32 -0300

    finished 04_feature_selection
    add the ramaining markdown cells and add SELECTED_FEATURES to config.py for future use

--- src/.ipynb_checkpoints/config-checkpoint.py
+++ src/.ipynb_checkpoints/config-checkpoint.py
@@ -1,2 +1,20 @@
 RANDOM_STATE = 42
 TEST_SIZE = 0.2
+
+SELECTED_FEATURES = [
+    "concave_points_mean",
+    "concavity_worst",
+    "symmetry_worst",
+    "radius_avg",
+    "perimeter_avg",
+    "area_avg",
+    "radius_mean",
+    "texture_mean",
+    "perimeter_mean",
+    "area_mean",
+    "smoothness_mean",
+    "compactness_mean",
+    "concavity_mean",
+    "symmetry_mean",
+    "fractal_dimension_mean"
+]
